0a83c809acd2997fa40baccdc9b88c5d013a2fec
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-10-22 18:01:20 -0400

    showhoot

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -30,4 +30,14 @@ router.get('/', async (req,res) => {
         res.status(500).json(error);
       }
 });
+
+router.get('/:hootId', async (req, res) => {
+    try {
+      const hoot = await Hoot.findById(req.params.hootId).populate('author');
+      res.status(200).json(hoot);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
 module.exports = router;
