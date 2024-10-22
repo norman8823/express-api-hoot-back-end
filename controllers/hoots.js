88c99fdb91d2754127c1b0cdf178c9b3e903a189
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-10-22 17:52:39 -0400

    updatehost

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -19,4 +19,30 @@ router.post('/', async (req, res)=>{
         res.status(500).json(error);
     }
 });
+router.put('/:hootId', async (req, res) => {
+    try {
+      // Find the hoot:
+      const hoot = await Hoot.findById(req.params.hootId);
+  
+      // Check permissions:
+      if (!hoot.author.equals(req.user._id)) {
+        return res.status(403).send("You're not allowed to do that!");
+      }
+  
+      // Update hoot:
+      const updatedHoot = await Hoot.findByIdAndUpdate(
+        req.params.hootId,
+        req.body,
+        { new: true }
+      );
+  
+      // Append req.user to the author property:
+      updatedHoot._doc.author = req.user;
+  
+      // Issue JSON response:
+      res.status(200).json(updatedHoot);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 module.exports = router;
