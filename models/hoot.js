417d415551cc7d8170f7f0073415b251203de1ac
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-10-22 17:21:50 -0400

    hoots create

--- models/hoot.js
+++ models/hoot.js
@@ -1,7 +1,16 @@
 // models/hoot.js
 
 const mongoose = require('mongoose');
-const Hoot = mongoose.model('Hoot', hootSchema);
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  },
+  { timestamps: true }
+);
 
 const hootSchema = new mongoose.Schema(
     {
@@ -24,15 +33,6 @@ const hootSchema = new mongoose.Schema(
     { timestamps: true }
   );
   
-  const commentSchema = new mongoose.Schema(
-    {
-      text: {
-        type: String,
-        required: true
-      },
-      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-    },
-    { timestamps: true }
-  );
-
+  
+  const Hoot = mongoose.model('Hoot', hootSchema);
   module.exports = Hoot
