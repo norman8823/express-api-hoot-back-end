30a987d6ff66675cf7c0db2549916438e85d2152
g g <gabedotgutierrez@gmail.com>
2024-10-24 09:58:39 -0400

    fixed post route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -5,12 +5,24 @@ const router = express.Router();
 
 // ========== Public Routes ===========
 
+router.get('/', async (req,res) => {
+  try {
+      const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+      res.status(200).json(hoots);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+});
+
 // ========= Protected Routes =========
 
 router.use(verifyToken);
+
 router.post('/', async (req, res)=>{
     try {
-        req.body.author =res.user_id;
+        req.body.author = req.user._id;
         const hoot = await Hoot.create(req.body);
         hoot._doc.author = req.user;
         res.status(201).json(hoot)
@@ -19,6 +31,7 @@ router.post('/', async (req, res)=>{
         res.status(500).json(error);
     }
 });
+
 router.put('/:hootId', async (req, res) => {
     try {
       // Find the hoot:
@@ -45,17 +58,6 @@ router.put('/:hootId', async (req, res) => {
       res.status(500).json(error);
     }
   });
-
-router.get('/', async (req,res) => {
-    try {
-        const hoots = await Hoot.find({})
-          .populate('author')
-          .sort({ createdAt: 'desc' });
-        res.status(200).json(hoots);
-      } catch (error) {
-        res.status(500).json(error);
-      }
-});
 
 router.get('/:hootId', async (req, res) => {
     try {
